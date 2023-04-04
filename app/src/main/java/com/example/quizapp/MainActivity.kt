2c493dfc619abdc59990b178b3f38e4f6446822f
Konrad Kolasa <k.kolasa@ideo.pl>
2023-04-04 14:49:10 +0200

    feat(initial): create Kotlin project with starting view and needed assets

---
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.quizapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        WindowInsetsControllerCompat(window, window.decorView).apply {
+            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            hide(WindowInsetsCompat.Type.systemBars())
+        }
+
+        val btnStart = findViewById<Button>(R.id.btn_start)
+        val etName = findViewById<AppCompatEditText>(R.id.user_name)
+        btnStart.setOnClickListener() {
+            if(etName.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            } else if (etName.text.toString().length < 3){
+                Toast.makeText(this, "Name should be at least 3 chars long", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+}

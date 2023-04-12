8cdc84862666cd12e111ed20fc8583da0f3a9f77
Konrad Kolasa <k.kolasa@ideo.pl>
2023-04-12 10:06:19 +0200

    feat(questions): connect UI with data model

--- app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -2,7 +2,9 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,25 @@ class QuizQuestionsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_quiz_questions)
 
         val questionsList = Constants.getQuestions()
+        val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
+        val tvProgress = findViewById<TextView>(R.id.tv_progress)
+        val questionText = findViewById<TextView>(R.id.tv_question_id)
+        val questionImage = findViewById<ImageView>(R.id.iv_image)
+        val questionOptionOne = findViewById<TextView>(R.id.tv_option_one)
+        val questionOptionTwo = findViewById<TextView>(R.id.tv_option_two)
+        val questionOptionThree = findViewById<TextView>(R.id.tv_option_three)
+        val questionOptionFour = findViewById<TextView>(R.id.tv_option_four)
+        val currentPosition = 1
+        val question: Question = questionsList[currentPosition - 1]
+
+        progressBar.progress = currentPosition
+        val progressText = resources.getString(R.string.progress, currentPosition, progressBar.max)
+        tvProgress.text = progressText
+        questionText.text = question.question
+        questionImage.setImageResource(question.image)
+        questionOptionOne.text = question.optionOne
+        questionOptionTwo.text = question.optionTwo
+        questionOptionThree.text = question.optionThree
+        questionOptionFour.text = question.optionFour
     }
 }
